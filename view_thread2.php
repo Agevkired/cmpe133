303760d332233e6034ec41044c9d494f23e76eac
emmendoza1986 <emmendoza1986@gmail.com>
2015-04-23 13:47:19 -0700

    forum thread development

--- view_thread2.php
+++ view_thread2.php
@@ -27,6 +27,81 @@
                 $forumRoot = $forumRoot->first();
 
 
+?>
+<div class="container"><!--Forum Display Start -->
+            <div class="row">
+                <div class="col-md-8">
+                  <div class="page-header">
+                    <h1><small class="pull-right">45 comments</small> Comments </h1>
+                  </div> 
+                   <div class="comments-list">
+                       <div class="media">
+                           <p class="pull-right"><small>5 days ago</small></p>
+                            <a class="media-left" href="#">
+                              <img src="http://lorempixel.com/40/40/people/1/">
+                            </a>
+                            <div class="media-body">
+                                
+                              <h4 class="media-heading user_name">Baltej Singh</h4>
+                              Wow! this is really great.
+                              
+                              <p><small><a href="">Like</a> - <a href="">Share</a></small></p>
+                            </div>
+                          </div>
+                       <div class="media">
+                           <p class="pull-right"><small>5 days ago</small></p>
+                            <a class="media-left" href="#">
+                              <img src="http://lorempixel.com/40/40/people/2/">
+                            </a>
+                            <div class="media-body">
+                                
+                              <h4 class="media-heading user_name">Baltej Singh</h4>
+                              Wow! this is really great.
+                              
+                              <p><small><a href="">Like</a> - <a href="">Share</a></small></p>
+                            </div>
+                          </div>
+                       <div class="media">
+                           <p class="pull-right"><small>5 days ago</small></p>
+                            <a class="media-left" href="#">
+                              <img src="http://lorempixel.com/40/40/people/3/">
+                            </a>
+                            <div class="media-body">
+                                
+                              <h4 class="media-heading user_name">Baltej Singh</h4>
+                              Wow! this is really great.
+                              
+                              <p><small><a href="">Like</a> - <a href="">Share</a></small></p>
+                            </div>
+                          </div>
+                       <div class="media">
+                           <p class="pull-right"><small>5 days ago</small></p>
+                            <a class="media-left" href="#">
+                              <img src="http://lorempixel.com/40/40/people/4/">
+                            </a>
+                            <div class="media-body">
+                                
+                              <h4 class="media-heading user_name">Baltej Singh</h4>
+                              Wow! this is really great.
+                              
+                              <p><small><a href="">Like</a> - <a href="">Share</a></small></p>
+                            </div>
+                          </div>
+                   </div>
+                    
+                    
+                    
+                </div>
+            </div>
+        </div><!--Forum Display End -->
+<?php
+
+
+
+
+
+
+
                 if($forumRoot){
                     $_SESSION['fid'] = $forumRootObjectId;
                     $forumRoot->fetch();
@@ -126,7 +201,7 @@
             <div class="col-md-6 col-md-push-3">
                 <div>
                     <?php displayForumThread()  //generateForumHere?>
-<div class="container">
+<div class="container"><!--Forum Display Start -->
             <div class="row">
                 <div class="col-md-8">
                   <div class="page-header">
@@ -191,7 +266,7 @@
                     
                 </div>
             </div>
-        </div>
+        </div><!--Forum Display End -->
                     <?php //if($_SESSION['fid']){ ?>
                     <form method="post" action="view_thread.php">
                     <textarea name="comment" cols="25" rows="5">
